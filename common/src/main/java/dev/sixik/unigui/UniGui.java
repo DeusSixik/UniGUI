92ef59eb8cadd714501fae68c12499912ba913b6
Bogdan <142807122+DeusSixik@users.noreply.github.com>
2026-09-01 15:32:46 +0300

    Update UniGui.java

--- common/src/main/java/dev/sixik/unigui/UniGui.java
+++ common/src/main/java/dev/sixik/unigui/UniGui.java
@@ -1,11 +1,8 @@
 package dev.sixik.unigui;
 
-import dev.sixik.unigui.api.text.Fonts;
-
 public final class UniGui {
     public static final String MOD_ID = "unigui";
 
     public static void init() {
-        Fonts.defaultFace();
     }
 }
